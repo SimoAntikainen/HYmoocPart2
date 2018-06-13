6d80f94127e44724135c63cfd606132a41b0bc96
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-13 19:12:47 +0300

    excersises 2.1-2.5

--- tutorial2/tutorial_two/src/App.js
+++ tutorial2/tutorial_two/src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Note from './components/Note'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Muistiinpanot</h1>
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note} />)}
+      </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
