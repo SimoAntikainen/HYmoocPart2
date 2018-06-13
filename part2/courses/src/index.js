76a52aa821bc02e2010a703982bfdbb463da44f8
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-13 19:46:58 +0300

    part 2.1

--- part2/courses/src/index.js
+++ part2/courses/src/index.js
@@ -1,8 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const Otsikko = (props) => {
+    return (
+      <h1>{props.course}</h1>
+    )}
+
+const Osa = ({content}) => {
+  return (
+    <li>{content.nimi} {content.tehtavia}</li>
+  )
+
+    
+}
+
+const Sisalto = ({contents}) => {
+  return (
+    <div>
+       <ul>
+        {contents.map(content => <Osa key={content.id} content={content} />)}
+      </ul>
+    </div>
+  )
+}
+
+const Yhteensa = (props) => {
+  return (
+    <p>yhteensä {props.sum_excercise[0].tehtavia + props.sum_excercise[1].tehtavia + props.sum_excercise[2].tehtavia} tehtävää</p>
+  )
+
+    
+}
+//<Yhteensa sum_excercise={kurssi.osat} />
+
+const Kurssi = ({kurssi}) => {
+  return (
+    <div>
+      <Otsikko course={kurssi.nimi}/>
+      <Sisalto contents={kurssi.osat}/>
+    </div>
+  )
+
+}
+
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat  : [{
+    nimi: 'Reactin perusteet',
+    tehtavia: 10,
+    id:1
+    
+  }, {
+    nimi: 'Tiedonvälitys propseilla',
+    tehtavia: 7,
+    id:2
+  }, {
+    nimi: 'Komponenttien tila',
+    tehtavia: 14,
+    id:3
+  }
+  ]
+  }
+
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,document.getElementById('root'))
