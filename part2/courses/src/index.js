f3628ec1c83be7b3b143505fc0b401f987fde861
Simo Antikainen <simo.antikainen@aalto.fi>
2018-06-13 20:25:12 +0300

    excersises 2.1-2.5

--- part2/courses/src/index.js
+++ part2/courses/src/index.js
@@ -1,49 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Otsikko = (props) => {
-    return (
-      <h1>{props.course}</h1>
-    )}
-
-const Osa = ({content}) => {
-  return (
-    <p>{content.nimi} {content.tehtavia}</p>
-  )
-
-    
-}
-
-const Sisalto = ({contents}) => {
-  return (
-    <div>       
-        {contents.map(content => <Osa key={content.id} content={content} />)}
-    </div>
-  )
-}
-
-const Yhteensa = ({contents}) => {
-  const reducerSum = (acc, cur) => acc + cur.tehtavia 
-  const summedExcersises = contents.reduce(reducerSum, 0) 
-  return (
-    <p>yhteensä {summedExcersises}</p>
-  )
-
-    
-}
-//<Yhteensa sum_excercise={kurssi.osat} />
-
-const Kurssi = ({kurssi}) => {
-  return (
-    <div>
-      <Otsikko course={kurssi.nimi}/>
-      <Sisalto contents={kurssi.osat}/>
-      <Yhteensa contents={kurssi.osat}/>
-    </div>
-  )
-
-}
-
+import Kurssi from './components/Kurssi'
 
 const App = () => {
   const kurssit = [
@@ -88,7 +45,7 @@ const App = () => {
 
   return (
     <div>
-      {kurssit.map(kurssi => <Kurssi key={kurssit.id} kurssi={kurssi} />)}
+      {kurssit.map(kurssi => <Kurssi key={kurssi.id} kurssi={kurssi} />)}
     </div>
   )
 }
